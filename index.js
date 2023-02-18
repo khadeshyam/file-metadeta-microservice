90301b6aa238d77cd73bfcfa90f7f878f0bf796a
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-18 13:38:26 +0530

    handle file upload using multer package

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 
 const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +14,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+//handle file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    const name = req.file.originalname;
+    const type = req.file.mimetype;
+    const size = req.file.size;
+    res.json({name,type,size});
+  } catch (err) {
+    res.send(400);
+  }
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
